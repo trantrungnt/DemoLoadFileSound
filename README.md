d53583e3b932cd71eaf9f1c50f6f19d19b1f3ced
Trần Trung <nguyentrantrung1986@gmail.com>
2016-03-13 23:37:30 -0700

    update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DemoLoadFileSound
+
+##Tham khảo
++ [Tham khảo Effect file âm thanh và Background file âm thanh](https://www3.ntu.edu.sg/home/ehchua/programming/java/J8c_PlayingSound.html)
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by TrungNT on 3/13/2016.
+ */
+import sun.audio.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+    }
+
+    public void static music()
+    {
+
+    }
+}
