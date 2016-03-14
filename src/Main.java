fef6a6d5862a19bdd0edaeb94f374a913e07bf7c
Trần Trung <nguyentrantrung1986@gmail.com>
2016-03-14 01:12:35 -0700

    demo load file wav in java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
+import java.net.URL;
+
 /**
  * Created by TrungNT on 3/13/2016.
  */
-import sun.audio.*;
+
 
 public class Main {
     public static void main(String[] args)
     {
+        SoundClipTest soundClipTest = new SoundClipTest();
+        soundClipTest.music();
 
     }
 
-    public void static music()
-    {
 
-    }
 }
---
+++ src/SoundClipTest.java
@@ -0,0 +1,43 @@
+/**
+ * Created by TrungNT on 3/13/2016.
+ */
+import javax.swing.*;
+import sun.audio.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class SoundClipTest extends JFrame{
+    BufferedImage sprite;
+
+    public SoundClipTest() {
+       /* try {
+            sprite = ImageIO.read(new File("resource\\pokemon.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Test Sound Clip");
+        this.setSize(452, 256);
+        this.setVisible(true);
+    }
+
+
+    public void music(){
+        AudioPlayer MGP = AudioPlayer.player;
+        AudioStream BGM;
+        AudioData MD;
+        ContinuousAudioDataStream loop = null;
+        try{
+            BGM = new AudioStream(new FileInputStream("resource\\pokerap.wav"));
+            MD = BGM.getData();
+            loop = new ContinuousAudioDataStream(MD);
+        }catch(IOException error){
+            System.out.print("file not found");
+        }
+
+        MGP.start(loop);
+    }
+
+
+}
